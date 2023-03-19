9ac2c5ee75917623071a6035a01254e5c9e79f67
PashaSeleznev <113536910+PashaSeleznev@users.noreply.github.com>
2023-03-19 19:31:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,69 +1,113 @@
+import cv2
 import time
 
-import cv2
+
+def ex_1():
+    img_1 = cv2.imread('image_1.jpg')
+    res = cv2.resize(img_1, dsize=(256, 256))
+    cv2.imshow('image', res)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+
+    big_image = cv2.resize(res, (0, 0), fx=2, fy=2)
+    cv2.imshow('image', big_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
 
 
-def image_processing():
-    img = cv2.imread('img_test.jpg')
-    #cv2.imshow('image', img)
-    w, h = img.shape[:2]
-    #(cX, cY) = (w // 2, h // 2)
-    #M = cv2.getRotationMatrix2D((cX, cY), 45, 1.0)
-    #rotated = cv2.warpAffine(img, M, (w, h))
-    #cv2.imshow('rotated', rotated)
-
-    #cat = img[250:580, 20:280]
-    #cv2.imshow('image', cat)
-
-    #r = cv2.selectROI(img)
-    #image_cropped = img[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
-    #cv2.imshow('cropped', image_cropped)
-
-    cv2.line(img, (0, 0), (580, 600), (255, 0, 0), 5)
-    cv2.rectangle(img, (384, 10), (580, 128), (0, 252, 0), 3)
-    cv2.putText(img, 'Lab. No 8', (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 3,
-                (0, 0, 255), 2, cv2.LINE_AA)
-    cv2.imshow('img', img)
-
-
-def video_processing():
-    cap = cv2.VideoCapture(1)
+def ex_2_3():
+    cap = cv2.VideoCapture(0)
     down_points = (640, 480)
-    i = 0
-    while True:
+    object_detector = cv2.createBackgroundSubtractorMOG2(varThreshold=100)
+    time_start = time.time()
+    cnt_left = 0
+    cnt_right = 0
+
+    while time.time() - time_start < 25:
         ret, frame = cap.read()
-        if not ret:
-            break
+        frame = cv2.resize(frame, down_points)
+        height, width, _ = frame.shape
 
-        frame = cv2.resize(frame, down_points, interpolation=cv2.INTER_LINEAR)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (21, 21), 0)
-        ret, thresh = cv2.threshold(gray, 110, 255, cv2.THRESH_BINARY_INV)
+        black = object_detector.apply(frame)
+        _, black = cv2.threshold(black, 253, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(black, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        contours, hierarchy = cv2.findContours(thresh,
-                            cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        if len(contours) > 0:
-            c = max(contours, key=cv2.contourArea)
-            x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            if i % 5 == 0:
-                a = x + (w // 2)
-                b = y + (h // 2)
-                print(a, b)
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            if area > 900:
+                cv2.drawContours(frame, [cnt], -1, (0, 0, 255), 2)
+                x, y, w, h = cv2.boundingRect(cnt)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                if x < 320 and x + w < 320:
+                    cnt_left += 1
+                if x > 320 and x + w > 320:
+                    cnt_right += 1
 
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        cv2.imshow("Frame", frame)
+        cv2.imshow("Black", black)
+        cv2.waitKey(1)
 
-        time.sleep(0.1)
-        i += 1
+    cap.release()
+
+    print('cnt_left = ', cnt_left)
+    print('cnt_right = ', cnt_right)
+
+
+def extra():
+    cap = cv2.VideoCapture(0)
+    img_2 = cv2.imread('fly64.jpg')
+
+    down_points = (640, 480)
+    object_detector = cv2.createBackgroundSubtractorMOG2(varThreshold=100)
+    time_start = time.time()
+    cnt_left = 0
+    cnt_right = 0
+
+    while time.time() - time_start < 25:
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, down_points)
+        height, width, _ = frame.shape
+
+        black = object_detector.apply(frame)
+        _, black = cv2.threshold(black, 253, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(black, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            if area > 900:
+                cv2.drawContours(frame, [cnt], -1, (0, 0, 255), 2)
+                x, y, w, h = cv2.boundingRect(cnt)
+
+                res = cv2.resize(img_2, dsize=(w, h))
+                cv2.imshow("Муха", res);
+                cv2.waitKey(1)
+                cv2.moveWindow("Муха", x, y)
+                time.sleep(0.1)
+                cv2.destroyWindow("Муха")
+
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                if x < 320 and x + w < 320:
+                    cnt_left += 1
+                if x > 320 and x + w > 320:
+                    cnt_right += 1
+
+        cv2.imshow("Frame", frame)
+        cv2.imshow("Black", black)
+        cv2.waitKey(1)
 
     cap.release()
 
 
-if __name__ == '__main__':
-    #image_processing()
-    video_processing()
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+ex_1()
+time.sleep(5)
+ex_2_3()
+time.sleep(2)
+# Перед запуском доп. задания я поместил окно в левый верхний угол.
+# Понимаю, что сделал не совсем то, что написано в задании.
+# Получилось реализовать лишь вариант с новым окном, в котором появляется изображение мухи.
+# Бывают ситуации, когда изображение мухи не в центре метки. Это связано с тем, что условия неидеальные.
+# В лучшем случае муха в рамке большого квадрата будет перекрывать изображения мух в более маленьком.
+# Поскольку иногда распознаются внутренние контуры метки, а не внешние, получается "муха в метке" или смещенная муха.
+extra()
